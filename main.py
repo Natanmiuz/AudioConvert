38cf47d858af1649c4353d491212a19f3d6f8761
Natanmiuz <natan.kimito@gmail.com>
2025-06-22 12:17:16 -0500

    Add file readme

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from gui.gui import AudioConverter
 
-# Corrección para que la GUI no se vea borrosa en pantallas con escalado (Windows)
+
 try:
     import ctypes
     ctypes.windll.shcore.SetProcessDpiAwareness(1)
